e66b9805fde91d900ba8458eb674eefbcac4ac77
MostafaHabib <sasahabeb86@gmail.com>
2023-11-19 18:14:30 +0200

    Initial commit

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Navigation = (props) => {
   return (
     <nav id="menu" className="navbar navbar-default navbar-fixed-top">
-      <div className="container">
+      <div className="header-container">
         <div className="navbar-header">
           <button
             type="button"
@@ -18,7 +18,7 @@ export const Navigation = (props) => {
             <span className="icon-bar"></span>{" "}
           </button>
           <a className="navbar-brand page-scroll" href="#page-top">
-            React Landing Page
+            Degital Web Creator
           </a>{" "}
         </div>
 
